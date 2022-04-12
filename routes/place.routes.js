04e8554d5679d7ef002ad154e0ea0df47e3edb89
Jeanet Lin <linett@gmail.com>
2022-04-12 20:51:15 +0200

    place routes CRUD done

--- routes/place.routes.js
+++ routes/place.routes.js
@@ -34,38 +34,48 @@ router.get("/myplaces", async (req, res) => {
 });
 
 
-//View one place by the user id
+//View one place by the place id
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
     const place = await Place.findById(id);
     res.status(200).json(place);
 });
 
-//Delete depending on the user id
-router.delete("/:id", async (req, res) => {
+//Delete depending on the place id
+router.delete("/:id", async (req, res) => {  
+    //grab the id from the route
     const { id } = req.params;
-    const place = await Place.findById(id);
-    if (place.user.toString() === req.jwtPayload.user._id){
-        await Place.findByIdAndDelete(id);
+    //we find the place we want to delete, we wait to check the author of the place
+    let place = await Place.findById(id); 
+    //we check if it is the owner of this place created, if the place matches the user in the jwt
+    if (place.author.toString() === req.jwtPayload.user._id) {
+        await Place.findByIdAndDelete(id)
         res.status(200).json(place);
     } else {
         res.status(400).json("Unauthorized");
     }
+  /*  try{
+    await Place.findByIdAndDelete(req.params.id);
+    res.status(200).json("This place has been deleted");
+   } catch (error) {
+       res.status(500).json(error)
+   } */
 });
+    
 
-//Edit depending on ID 
+//Edit depending on place id  
 router.put("/:id", async (req, res) => {
     const { id } = req.params;
     const { name, address, city, dietaryType, description } = req.body;
     let place = await Place.findById(id);
-    if (place.user.toString() === req.jwtPayload.user._id) {
+    if (place.author.toString() === req.jwtPayload.user._id) {
         place.name = name;
         place.address = address;
         place.city = city;
         place.dietaryType = dietaryType;
         place.description = description;
         place = await place.save();
-        res.status(200).json(tweet);
+        res.status(200).json(place);
     } else {
         res.status(400).json("Unauthorized");
     }
